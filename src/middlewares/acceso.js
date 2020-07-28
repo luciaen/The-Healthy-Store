15945bd818bcbba9566895817f9cedb74a8acac3
FedericoCuello <cuellofederico94@gmail.com>
2020-07-28 01:05:44 -0300

    Se arregla problema con el update

--- src/middlewares/acceso.js
+++ src/middlewares/acceso.js
@@ -11,9 +11,9 @@ module.exports = (req,res,next) =>{
     }else if(req.cookies.email){
         let usuario = usuarios.find(u => u.email == req.cookies.email)
         //return res.send(usuario);
-        /*if(usuario.password != "undefined"){
+        if(usuario.password != "undefined"){
             delete usuario.password;
-        }*/
+        }
         req.session.usuario = usuario;
         res.locals.usuario = usuario;
         return next();
