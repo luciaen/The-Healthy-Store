4bdc712124b7a3a91442084e149880d14a952f92
FedericoCuello <cuellofederico94@gmail.com>
2020-08-08 13:08:39 -0300

    Se cambia el ojito del password

--- public/js/ojito.js
+++ public/js/ojito.js
@@ -1,16 +1,18 @@
 window.addEventListener('load', function () {
-    let show = document.getElementById("show")
+    let show = document.getElementById("show_password")
     //console.log(hola)
     show.addEventListener("mousedown",function(){
         let pass = document.getElementById("pass");
         pass.removeAttribute('type');
-        show.classList.add('fa-eye-slash')
-        show.classList.remove('fa-eye')
+        show.classList.remove('fas fa-eye-slash')
+        show.classList.add('fas fa-eye')
+        
     })
     show.addEventListener("mouseup",function(){
         let pass = document.getElementById("pass");
         pass.setAttribute('type','password');
-        show.classList.add('fa-eye')
-        show.classList.remove('fa-eye-slash')
+        show.classList.remove('fas fa-eye')
+        show.classList.add('fas fa-eye-slash')
+        
     })
 })
