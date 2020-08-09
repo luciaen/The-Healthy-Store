74ff801ace18a2846b5ebd66b4d83faf603b9684
FedericoCuello <cuellofederico94@gmail.com>
2020-08-09 12:00:34 -0300

    se modifica el modelo user

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, dataTypes) => {
             email: dataTypes.STRING,
             phone: dataTypes.INTEGER,
             password: dataTypes.STRING,
+            admin:dataTypes.INTEGER,
             image: dataTypes.STRING
         };
 
